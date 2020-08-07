a838051e368c8537d399ad8eb141ba066dc8ac28
OmarInCS <omarincs@gmail.com>
2020-08-07 23:12:52 +0300

    session part 1

--- week3/week3-4.js
+++ week3/week3-4.js
@@ -1,4 +1,9 @@
-let x, y, score;
+let x, y, score, scoreHistory;
+let scoreRecordTemplate = `
+                <div class="history-item">
+                    <span class="date">{{date}}</span><span class="score">{{score}}</span>
+                </div>
+`;
 
 function preprocess() {
     displayQuestion();
@@ -7,6 +12,7 @@ function preprocess() {
     for (let tab of tabList) {
         tab.onclick = tabPressed;
     }
+    scoreHistory = JSON.parse(localStorage.getItem("scoreHistory")) || [];
 }
 
 function displayQuestion() {
@@ -30,6 +36,12 @@ function checkAnswer() {
     }
     else {
         document.getElementById("message").innerText = "Game Over";
+        document.getElementById("answer").disabled = true;
+        document.getElementById("bt-check").disabled = true;
+        let options = {year: "numeric", month: "long", day: "numeric"};
+        let historyRecord = {date: new Date().toLocaleDateString("en-UK", options), score: score};
+        scoreHistory.push(historyRecord);
+        localStorage.setItem("scoreHistory", JSON.stringify(scoreHistory));
     }
 }
 
@@ -37,12 +49,21 @@ function playAgain() {
     document.getElementById("message").innerText = "";
     score = 0;
     document.querySelector(".box > h3").innerHTML = `Score: 0`;
+    document.getElementById("answer").disabled = false;
+    document.getElementById("bt-check").disabled = false;
     displayQuestion();
 }
 
 
 function displayHistory() {
-    
+    console.log(scoreHistory);
+    let historyTag = document.getElementById("history");
+    historyTag.innerHTML = "";
+    for (let score of scoreHistory) {
+        let scoreRecord = scoreRecordTemplate.replace("{{date}}", score.date)
+                                             .replace("{{score}}", score.score);
+        historyTag.innerHTML += scoreRecord;
+    }
 }
 
 
@@ -61,4 +82,8 @@ function tabPressed() {
         box.classList.add("hidden");
     }
     boxList[idx].classList.remove("hidden");
+
+    if (this.innerText === "History") {
+        displayHistory();
+    }
 }
